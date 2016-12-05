5032be877e76c15c376b6bca77f729fee9570aef
Kyle Marshall <kylemarshall88@gmail.com>
2016-12-04 23:59:25 -0800

    testing of get_data func in test_utils

--- scripts/test_utils.py
+++ scripts/test_utils.py
@@ -3,9 +3,17 @@ __doc__ = """
 	Testing util functionality
 
 """
-from Two_Sigma_Financial_Modeling_Challenge.utils import data_utils
+import os
+import sys
+import imp
+PROJECT_DIR = os.path.dirname(
+			os.path.dirname(
+				os.path.realpath(__file__)))
 
-data_set = get_data()
+data_utils_path = os.path.join(PROJECT_DIR,'utils','data_utils.py')
+data_utils = imp.load_source('data_utils', data_utils_path)
+import data_utils
+data_set = data_utils.get_data()
 print data_set.keys()
 df = data_set.to_df()
 print df.head()
