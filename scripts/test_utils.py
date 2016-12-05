050eb3192b22d976d61e5a7440ed9846c8808e09
Kyle Marshall <kylemarshall88@gmail.com>
2016-12-04 23:35:53 -0800

    added a few init files; testing get_data util function

--- scripts/test_utils.py
+++ scripts/test_utils.py
@@ -3,14 +3,6 @@ __doc__ = """
 	Testing util functionality
 
 """
-import sys
-import os
-
-PROJECT_DIR = os.path.dirname(
-				os.path.dirname(
-					os.path.realpath(__file__)))
-sys.path.append(PROJECT_DIR)
-
 from Two_Sigma_Financial_Modeling_Challenge.utils import data_utils
 
 data_set = get_data()
