4235523e11b48e4793ebfa956da6cfe18f5ea251
jstaker7 <jstaker7@gmail.com>
2017-01-12 16:58:03 -0800

    Added simple test script to test mock_gym

---
+++ utils/mock_gym_test.py
@@ -0,0 +1,22 @@
+from utils import mock_gym as kagglegym
+
+env = kagglegym.make()
+observation = env.reset()
+
+print(len(observation.target))
+print(len(observation.train))
+
+n = 0
+rewards = []
+while True:
+    target = observation.target
+    target.loc[:, 'y'] = 0.01
+    observation, reward, done, info = env.step(target)
+    if done:
+        break
+    rewards.append(reward)
+    n = n + 1
+
+print(info)
+print(n)
+print(rewards[0:15])
